d2537dba7d55f116881fe8e67018625bb3252daf
surya-siddhi-sai-2710 <107804010+surya-siddhi-sai-2710@users.noreply.github.com>
2023-05-27 10:45:16 +0530

    FloydsTriangle.java

--- FloydsTriangle.java
+++ FloydsTriangle.java
@@ -15,7 +15,6 @@ public class FloydsTriangle
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 5;
-        int m = 5;
         int k = 1;
         for (int i = 1; i <= n; i++){
             for(int j=1; j<=i; j++){
