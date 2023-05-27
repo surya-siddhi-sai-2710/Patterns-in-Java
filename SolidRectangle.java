9663b5b24db59d743262ca6279789bbecf9d5e3b
surya-siddhi-sai-2710 <107804010+surya-siddhi-sai-2710@users.noreply.github.com>
2023-05-27 10:03:59 +0530

    SolidRectangle.java

---
+++ SolidRectangle.java
@@ -0,0 +1,26 @@
+/*
+To print the following pattern:
+
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+
+*/
+
+import java.util.*;
+
+public class SolidRectangle
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+        int m = 5;
+        for (int i = 1; i <= n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
