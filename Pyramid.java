6365f47014d301455cf3059cd0744b089760bff8
surya-siddhi-sai-2710 <107804010+surya-siddhi-sai-2710@users.noreply.github.com>
2023-05-27 10:10:21 +0530

    Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,24 @@
+/*
+To print the following pattern:
+
+* 
+* *
+* * *
+* * * *
+
+*/
+import java.util.*;
+
+public class Pyramid
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+        for (int i = 1; i <= n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+                }
+            System.out.println();
+        }
+    }
+}
