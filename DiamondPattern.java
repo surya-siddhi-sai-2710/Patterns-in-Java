9dd38ea8411f111c86ac93a5edbad78aabe939d0
surya siddhi sai <107804010+surya-siddhi-sai-2710@users.noreply.github.com>
2023-05-28 11:15:44 +0530

    DiamondPattern.java

---
+++ DiamondPattern.java
@@ -0,0 +1,50 @@
+/*
+output:
+   *
+  ***
+ *****
+*******
+*******
+ *****
+  ***
+   *
+
+*/
+import java.util.*;
+
+public class DiamondPattern
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+
+        for (int i = 1; i <= n; i++){
+            int spaces = n - i;
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=i; j>=1; j--){
+                System.out.print("*");
+            }
+        
+            for(int j=2; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        for(int i=n; i>=1 ; i--){
+            int spaces = n - i;
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=i; j>=1; j--){
+                System.out.print("*");
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
