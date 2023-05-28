955cdb5efd008f8a180b0c63f71ae913c627003c
surya siddhi sai <107804010+surya-siddhi-sai-2710@users.noreply.github.com>
2023-05-28 10:30:30 +0530

    SolidRhombus.java

---
+++ SolidRhombus.java
@@ -0,0 +1,29 @@
+/*
+output:
+        * * * * * 
+      * * * * *
+    * * * * *
+  * * * * *
+* * * * *
+
+*/
+import java.util.*;
+
+public class SolidRhombus
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        for (int i = 1; i <= n; i++){
+            int spaces = n - i;
+            for(int j = 1; j<=spaces; j++){
+                System.out.print("  ");
+            }
+
+            for(int j=1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
